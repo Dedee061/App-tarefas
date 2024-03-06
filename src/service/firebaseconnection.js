d9f3cbfb1a62838dfb6075f3b51b852fd8ed1348
Andre Santos <110115765+Dedee061@users.noreply.github.com>
2024-03-06 17:35:31 -0300

    Update firebaseconnection.js

--- src/service/firebaseconnection.js
+++ src/service/firebaseconnection.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfNm_IVqtWzkUEUc7xrlVFJ8zZrz_oCe8",
-    authDomain: "app-tarefas-82d93.firebaseapp.com",
-    projectId: "app-tarefas-82d93",
-    storageBucket: "app-tarefas-82d93.appspot.com",
-    messagingSenderId: "235942277714",
-    appId: "1:235942277714:web:a98a09b59371a8fb95e58b",
-    measurementId: "G-EPHVTSWZ8E"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   if(!firebase.apps.length) {
